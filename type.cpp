aca31435a431180f3413eda67fa1a382db436498
shaambhavi dubey <shaambhavi.vda@gmail.com>
2025-08-08 23:22:25 +0530

    Add files via upload

---
+++ type.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    int x;
+    int y;
+    std::cout<<"enter first number: ";
+    std::cin>>x;
+    std::cout<<"enter second number: ";
+    std::cin>>y;
+    std::cout<<"the sum is "<<x+y;
+    return 0;
+
+}
