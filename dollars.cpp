a88f6eb843e53c0992f4f17d824488b07ae50cc6
shaambhavi dubey <shaambhavi.vda@gmail.com>
2025-08-08 23:32:03 +0530

    Add files via upload

---
+++ dollars.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    int doll;
+    int ruppee;
+    std::cout<<"enter money in dollars";
+    std::cin>>doll;
+    ruppee=doll*48;
+    std::cout<<"money in ruppees is"<<ruppee;
+    return 0;
+
+
+}
