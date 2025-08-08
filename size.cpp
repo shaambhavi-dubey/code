b448fd7a5ab20d033f35c314d859f6466489999e
shaambhavi dubey <shaambhavi.vda@gmail.com>
2025-08-08 23:48:21 +0530

    Add files via upload

---
+++ size.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a;
+    float b;
+    cout<<"enter an integer";
+    cin>>a;
+    cout<<"enteradecimal";
+    cin>>b;
+    cout<<"size of a is"<<sizeof(a);
+    cout<<"size of b is"<<sizeof(b);
+    return 0;
+
+}
